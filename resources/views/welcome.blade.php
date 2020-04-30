2883dc898558c7f4e87a219c59a4f76de6f36a6e
nakashi <coffeelike.ncnn@gmail.com>
2020-04-30 06:36:54 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Auth::check())
-        {{ Auth::user()->name }}
+     @if (Auth::check())
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
