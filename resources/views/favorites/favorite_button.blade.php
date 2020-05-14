4d73f2ae477f5f4bc2c517af51bc54950b171bc1
nakashi <coffeelike.ncnn@gmail.com>
2020-05-14 02:06:52 +0000

    favorite btn

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::user()->is_favoriting($micropost->id))
     {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('UnFavorite', ['class' => "btn btn-danger btn-block"]) !!}
+        {!! Form::submit('UnFavorite', ['class' => "btn btn-warning btn-sm"]) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-        {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
     {!! Form::close() !!}
 @endif
